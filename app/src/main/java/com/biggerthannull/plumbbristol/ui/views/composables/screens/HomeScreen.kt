ea70c928577935db54953cce30868aa9e09697c3
AndyR <andrejs.riz@googlemail.com>
2024-01-17 11:31:06 +0000

    slight ui tweak for the list

--- app/src/main/java/com/biggerthannull/plumbbristol/ui/views/composables/screens/HomeScreen.kt
+++ app/src/main/java/com/biggerthannull/plumbbristol/ui/views/composables/screens/HomeScreen.kt
@@ -1,13 +1,14 @@
 package com.biggerthannull.plumbbristol.ui.views.composables.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
@@ -20,8 +21,8 @@ import com.biggerthannull.plumbbristol.ui.views.viewmodels.state.HomeScreenUISta
 fun HomeScreen(uiState: HomeScreenUIState, navController: NavController) {
     Column(
         modifier = Modifier
+            .background(Color.DarkGray)
             .fillMaxWidth()
-            .wrapContentSize(Alignment.Center)
     ) {
         if (uiState.bathrooms.isNotEmpty()) {
             PrimaryListComponent(data = uiState.bathrooms) { itemId ->
@@ -29,7 +30,11 @@ fun HomeScreen(uiState: HomeScreenUIState, navController: NavController) {
                 navController.navigate(napPath)
             }
         } else {
-            Text(text = stringResource(id = R.string.generic_error_label))
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center,
+                text = stringResource(id = R.string.generic_error_label)
+            )
         }
     }
 }
